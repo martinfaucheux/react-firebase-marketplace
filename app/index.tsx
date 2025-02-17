b8cbc4a5a498a27120c5262cff3def66e4c9daca
Martin Faucheux <martin.faucheux@gmail.com>
2025-02-17 16:37:52 +0100

    change style definition

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
 import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen. This is new.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
